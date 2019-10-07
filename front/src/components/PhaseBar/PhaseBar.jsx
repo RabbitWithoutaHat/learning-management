31695611b534f3a10b82a3a2456cef2824848f62
Fekla <rabbdroid@gmail.com>
2019-10-07 11:18:44 +0300

    front merge

--- front/src/components/PhaseBar/PhaseBar.jsx
+++ front/src/components/PhaseBar/PhaseBar.jsx
@@ -1,98 +1,77 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import { getNewsData } from '../../redux/News/action';
-import { connect } from "react-redux";
-import { Route, Link, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+// import { Route, Link, Redirect } from 'react-router-dom';
 import { getTopicsData } from '../../redux/Lections/actions';
+import { Tab } from 'semantic-ui-react';
 
-class PhaseBar extends Component {
-  //   async componentDidUpdate(prevProps) {
-  //     if (prevProps !== this.props) {
+// class PhaseBar extends Component {
+//   //   async componentDidUpdate(prevProps) {
+//   //     if (prevProps !== this.props) {
 
-  //       console.log(this.props.userName);
-  //  const userName = this.props.userName;
-  //       const resp = await fetch('/gettopics')
-  //       const data = await resp.json();
-  //       console.log(data);
-  //     }
-  //   }
-  state = {
-    mass: [],
-    curarr: [],
-    phase1: [],
-    phase2:[],
-  }
-  onlyUnique = (value, index, self) => {
-    return self.indexOf(value) === index;
-  }
-  async componentDidMount() {
-    await this.props.getTopics();
-    const arr = [1, 1, 1, 2, 34, 3, 3, 5, 7];
-    // console.log(this.props.topics);
-    this.setState({ mass: this.props.topics });
-    // const unique = arr.filter(onlyUnique);
-    // console.log(this.state.mass);
+//   //       console.log(this.props.userName);
+//   //  const userName = this.props.userName;
+//   //       const resp = await fetch('/gettopics')
+//   //       const data = await resp.json();
+//   //       console.log(data);
+//   //     }
+//   //   }
+//   // state = {
+//   //   mass: [],
+//   //   curarr: [],
+//   //   phase1: [],
+//   //   phase2: [],
+//   // };
+//   onlyUnique = (value, index, self) => {
+//     return self.indexOf(value) === index;
+//   };
+//   async componentDidMount() {
+//     await this.props.getTopics();
+//     const arr = [1, 1, 1, 2, 34, 3, 3, 5, 7];
+//     // console.log(this.props.topics);
+//     this.setState({ mass: this.props.topics });
+//     // const unique = arr.filter(onlyUnique);
+//     // console.log(this.state.mass);
 
-    // const arr2 = this.props.topics;
-    // console.log(arr2);
-    const phase1 = this.state.mass.filter(el => el.phase === '1')
-    this.setState({ phase1: phase1 })
-    // .sort(el=>el.week);
-    const phase2 = this.state.mass.filter(el => el.phase === '2');
-    this.setState({ phase2: phase2 })
-    // console.log('phase1', phase1);
+//     // const arr2 = this.props.topics;
+//     // console.log(arr2);
+//     const phase1 = this.state.mass.filter(el => el.phase === '1');
+//     this.setState({ phase1: phase1 });
+//     // .sort(el=>el.week);
+//     const phase2 = this.state.mass.filter(el => el.phase === '2');
+//     this.setState({ phase2: phase2 });
+//     // console.log('phase1', phase1);
+//   }
 
-  }
+//   render() {
+//     return (
+//       <div>
+//         <TabExampleBasic />
+//       </div>
+//     );
+//   }
+// }
 
-  render() {
-    // console.log(this.props.userName);
+const panes = [
+  { menuItem: 'Tab 1', render: () => <Tab.Pane>Tab 1 Content</Tab.Pane> },
+  { menuItem: 'Tab 2', render: () => <Tab.Pane>Tab 2 Content</Tab.Pane> },
+  { menuItem: 'Tab 3', render: () => <Tab.Pane>Tab 3 Content</Tab.Pane> },
+];
 
-    return (
-      <div>
-        {/* <div>
-          <ul>
-            Phase1
-        {this.state.phase1.map((el) => {
+const PhaseBar = () => <Tab panes={panes} />;
 
-              return <ul> Nedelia:{el.week}
-              {el.week.map((el) => {
-
-                return  <li>
-                Day :{el.day}
-              </li>
-              })}
-             
-              
-               </ul>
-            })}
-
-          </ul>
-
-
-        </div>
-        <div>
-          <ul>
-            Phase1
-        {this.state.phase2.map((el) => {
-
-              return <li>{el.week} </li>
-            })}
-
-          </ul>
-        </div> */}
-      </div>
-    )
-  }
-}
-
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return {
     topics: state.Topics.topics,
     userName: state.User.user.login,
-  }
+  };
 };
-const mapDispatchToProps = (dispatch) => {
+const mapDispatchToProps = dispatch => {
   return {
-    getTopics: () => dispatch(getTopicsData())
-  }
+    getTopics: () => dispatch(getTopicsData()),
+  };
 };
-export default connect(mapStateToProps, mapDispatchToProps)(PhaseBar)
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(PhaseBar);
