121ed36fd81d2aeca2b4e2c9e579054a96b45bec
Fekla <rabbdroid@gmail.com>
2019-10-09 17:25:11 +0300

    refactoring start

--- front/src/components/PhaseBar/PhaseBar.jsx
+++ front/src/components/PhaseBar/PhaseBar.jsx
@@ -6,110 +6,91 @@ import { getTopicsData } from '../../redux/Lections/actions';
 import { List } from 'semantic-ui-react';
 import { Tabs, TabList, Tab, TabPanel, CustomTab } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
-import { Button } from 'semantic-ui-react'
-import { Select } from 'semantic-ui-react'
-
-
+import { Button } from 'semantic-ui-react';
+import { Select } from 'semantic-ui-react';
 
 class PhaseBar extends Component {
   state = {
-    tabIndex:0,
-    active: false, values: [], 
+    tabIndex: 0,
+    active: false,
+    values: [],
     search: '',
-    countryOptions : [
+    countryOptions: [
       { key: 'af', value: 'af', text: 'Afghanistan' },
       { key: 'ax', value: 'ax', text: 'Aland Islands' },
-     
     ],
-    selectedGroupName:'',
-    groupNames:'',
+    selectedGroupName: '',
+    groupNames: '',
   };
   async componentDidMount() {
     await this.props.getTopics();
-    
-    console.log(this.props.topics.length);
   }
-addPhase= async() => {
-  let resp = await fetch('/addphase')
-  let data = await resp.json();
-  await this.props.getTopics();
-}
-getSelecetedGroup = async(event, {value}) => {
-  console.log('VALUE!!',value);
-  let selectedGroup = event.target.textContent;
-  console.log('text!!',selectedGroup);
-  await this.props.getTopics(selectedGroup);
-}
+  addPhase = async () => {
+    let resp = await fetch('/addphase');
+    let data = await resp.json();
+    await this.props.getTopics();
+  };
+  getSelecetedGroup = async (event, { value }) => {
+    console.log('VALUE!!', value);
+    let selectedGroup = event.target.textContent;
+    console.log('text!!', selectedGroup);
+    await this.props.getTopics(selectedGroup);
+  };
   render() {
-  
-
-    // console.log(this.state.data);
     return (
       <>
-      <Select className='select' placeholder='Select your country' options={this.props.groupNames} 
-       onChange={this.getSelecetedGroup}
-      />
-     
-      {/* {()=>console.log(this.state.values)} */}
-      {/* <div>erfwefwefwefew {this.state.values}</div> */}
-     {/* <Select active={this.state.active}
-         selection
-         selected={this.state.values}
-         placeholder="Select me"
-         onSelectChange={val => this.setState({values: val, active: false})}
-         onClick={() => this.setState({active: !this.state.active})}
-         onRequestClose={() => this.setState({active: false})}
- >
-     <Option >First</Option>
-     <Option >Second</Option>
- </Select> */}
- <div>{this.state.tabIndex}</div>
-      <Tabs
-        selectedIndex={this.state.tabIndex}
-        onSelect={tabIndex => this.setState({ tabIndex })}
-        className="phaseTabs"
-      >
-        <div>
-        </div>
-        <TabList>
-          {this.props.topics ? this.props.topics.map((phase, i) => <Tab key={`${i}tabs`}>Фаза {i + 1}</Tab>) : <p></p>}
-          <Tab onClick={this.addPhase}>+</Tab>
-        </TabList>
-        {this.props.topics ? (
-          this.props.topics.map((phase, i) => (
-            <TabPanel key={`${i}phase`}>
-              {phase.map((week, i) => (
-                <>
-                  <h3 className="weekTitle" key={`${i}week`}>
-                    Неделя {week[0].week}
-                  </h3>
-                  <List>
-                    {week.map((day, i) => (
-                      <List.Item key={`${i}day`}>
-                        <Link params={{ desc: day.description }} to={`/lections/${day._id}`}>
-                          {day.topicName}
-                        </Link>
-                      </List.Item>
-                      
-                    ))}
-                  </List>
-                  <Button positive>+</Button>
-                </>
-              ))}
-            </TabPanel>
-          ))
-        ) : (
+        <Select
+          className="select"
+          placeholder="Select your country"
+          options={this.props.groupNames}
+          onChange={this.getSelecetedGroup}
+        />
+        <Tabs
+          selectedIndex={this.state.tabIndex}
+          onSelect={tabIndex => this.setState({ tabIndex })}
+          className="phaseTabs"
+        >
+          <div></div>
+          <TabList>
+            {this.props.topics ? (
+              this.props.topics.map((phase, i) => <Tab key={`${i}tabs`}>Фаза {i + 1}</Tab>)
+            ) : (
+              <p></p>
+            )}
+            <Tab onClick={this.addPhase}>+</Tab>
+          </TabList>
+          {this.props.topics ? (
+            this.props.topics.map((phase, i) => (
+              <TabPanel key={`${i}phase`}>
+                {phase.map((week, i) => (
+                  <>
+                    <h3 className="weekTitle" key={`${i}week`}>
+                      Неделя {week[0].week}
+                    </h3>
+                    <List>
+                      {week.map((day, i) => (
+                        <List.Item key={`${i}day`}>
+                          <Link params={{ desc: day.description }} to={`/lections/${day._id}`}>
+                            {day.topicName}
+                          </Link>
+                        </List.Item>
+                      ))}
+                    </List>
+                    <Button basic color="violet" icon="plus"></Button>
+                  </>
+                ))}
+              </TabPanel>
+            ))
+          ) : (
             <p></p>
           )}
- <Button positive>Add week</Button>
+          <Button className="addWeek" basic color="violet" icon="plus">
+            Добавить неделю
+          </Button>
 
- <div>{this.props.status?
- <div>ti admin</div>
- :
- <div>ti lox</div>
- }</div>
-      </Tabs>
-     </>
+          <div>{this.props.status ? <div></div> : <div>ti lox</div>}</div>
+        </Tabs>
+      </>
     );
   }
 }
@@ -119,15 +100,15 @@ const mapStateToProps = state => {
 
   return {
     topics: state.Topics.topics,
-    groupNames:state.Topics. groupNames,
-    selectedGroupName:state.Topics.selectedGroupName,
+    groupNames: state.Topics.groupNames,
+    selectedGroupName: state.Topics.selectedGroupName,
     userName: state.User.user.login,
-    status:state.User.user.adminstatus,
+    status: state.User.user.adminstatus,
   };
 };
 const mapDispatchToProps = dispatch => {
   return {
-    getTopics: (selectedGroup) => dispatch(getTopicsData(selectedGroup)),
+    getTopics: selectedGroup => dispatch(getTopicsData(selectedGroup)),
   };
 };
 export default connect(
--- front/src/components/TestList/TestList.jsx
+++ front/src/components/TestList/TestList.jsx
@@ -5,7 +5,6 @@ import { Icon, List } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 
 class TestList extends Component {
-
   componentDidMount() {
     this.props.getTests();
   }
@@ -15,39 +14,42 @@ class TestList extends Component {
       <>
         {console.log(this.props)}
 
-        <List className="ui massive relaxed list usersList">
-          {this.props.tests ? this.props.tests.map((e, i) =>
-            <List.Item key={`${i}test`} className="item">
-              <List.Content className="content">
-                <Icon className="question" />
-                <Link onClick={formId => this.setState({ formId })} to={`/tests/${e.googleFormsLink}`}>
-                  {e.title}
-
-                </Link>
-                {this.props.user.status ?
-                  <p>admin</p> : <p>NET!</p>}
-              </List.Content>
-            </List.Item>
-          ) : <></>}
+        <List className="ui massive  list testsList">
+          {this.props.tests ? (
+            this.props.tests.map((e, i) => (
+              <List.Item key={`${i}test`} className="item testItem">
+                <List.Content className="content">
+                  <Icon className="question" />
+                  <Link onClick={formId => this.setState({ formId })} to={`/tests/${e.googleFormsLink}`}>
+                    {e.title}
+                  </Link>
+                  {this.props.user.status ? <p>admin</p> : <p>NET!</p>}
+                </List.Content>
+              </List.Item>
+            ))
+          ) : (
+            <></>
+          )}
         </List>
-
       </>
-    )
+    );
   }
-
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return {
     tests: state.Test.tests,
-    user: state.User.user
-  }
+    user: state.User.user,
+  };
 };
 
-const mapDispatchToProps = (dispatch) => {
+const mapDispatchToProps = dispatch => {
   return {
-    getTests: () => dispatch(getAllTests())
-  }
+    getTests: () => dispatch(getAllTests()),
+  };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(TestList)
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(TestList);
